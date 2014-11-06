2a748bcd27a34001be399ca48cd1502f6b45a567
Drew Wells <drew.wells00@gmail.com>
2014-11-06 01:37:23 -0600

    Through the magic of reflection, call yet break good funcs map[string]interface {}{     "data": []interface {}{         map[string]interface {}{             "dateCreated":       float64(1.415259386119e+12),             "pairingExpiration": float64(1.415345786119e+12),             "pairingCode":       "RgnmWWS",             "policies":          []interface {}{                 map[string]interface {}{                     "policy": "id",                     "method": "inactive",                     "params": []interface {}{                         "TfEPd2Dx2Va9PWYJmYmu8YTm4qVL8mktLkj",                     },                 },             },             "token": "ArbUrwZxDCzoMKbR8Sa3QLJSZpUePse3iY6Pv9dGAYYa",             "label": "node-bitpay-client-dwells-mac2",         },     }, }map[string]interface {}{     "error": "Unable to create token because of too many requests.", }

--- bitpay/main.go
+++ bitpay/main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"reflect"
 
 	bitpay "github.com/drewwells/go-bitpay-client"
 	"github.com/kr/pretty"
 )
 
 func main() {
-	resp := bitpay.Rates()
+	flag.Parse()
+	var resp []byte
+	iresp := bitpay.Token()
+
+	// Create a list of available commands and if found call them
+	funcs := map[string]interface{}{
+		"token":   bitpay.Token,
+		"invoice": bitpay.Invoice,
+		"bills":   bitpay.Bills,
+		"bill":    bitpay.Bill,
+	}
+
+	for _, v := range flag.Args() {
+		if addr, ok := funcs[v]; ok {
+			f := reflect.ValueOf(addr)
+			r := f.Call([]reflect.Value{})
+			resp = (r[0]).Bytes()
+		}
+	}
 	// JSON is returned
-	var v interface{}
+	var v, vv interface{}
 	json.Unmarshal(resp, &v)
+	json.Unmarshal(iresp, &vv)
+	fmt.Printf("% #v", pretty.Formatter(vv))
 	fmt.Printf("% #v", pretty.Formatter(v))
 }
