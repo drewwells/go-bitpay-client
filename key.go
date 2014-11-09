f43509bfb17d0fc22b6e124f25622f17ef19e623
Drew Wells <drew.wells00@gmail.com>
2014-11-09 14:46:11 -0600

    work towards generating pub keys from priv keys

--- key.go
+++ key.go
@@ -1,54 +1,50 @@
 package go_bitpay_client
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"math/big"
 
 	btcaddr "github.com/drewwells/gimme_bitcoin_address"
 )
 
-// Keygen generates a new private/public key pair.  These are used for pairing
-// to the Bitpay API.
+// Keygen generates a new private/public key pair using Bitcoin scheme
 func Keygen() ([]byte, []byte, error) {
 
 	p, b, err := btcaddr.Bitcoin_GenerateKeypair()
-	_ = b
-	fmt.Printf("Bitcoin len: %d\n", len(p.D.Bytes()))
+	//fmt.Printf("Bitcoin len: %d\n", len(p.D.Bytes()))
 	//privstr := gimme.Bitcoin_Prikey2WIF(p)
-
-	fmt.Printf("int  %d %x\n", len(p.D.Bytes()), p.D.Bytes()) //THIS IS IT FUCK THESE ENCODING SCHEMAS
+	priv := p.D.Bytes()
+	//fmt.Printf("int  %d %x\n", len(p.D.Bytes()), p.D.Bytes()) //THIS IS IT
 	//fmt.Printf("% #v\n", pretty.Formatter(b))
-	pub := Public(b.X, b.Y)
-	fmt.Printf("pub  %d %x\n", len(pub), pub)
-	//fmt.Printf("pub %d %x\n", len(b.R.Bytes()), b.R.Bytes())
+	pub := public(b.X, b.Y)
+	//fmt.Printf("pub  %d %x\n", len(pub), pub)
 
-	//RIPEMD160 encode the int
-	/*ripe := ripemd160.New()
-	privhash := ripe.Sum(p.D.Bytes())
-	fmt.Printf("hash %d %x\n", len(privhash), privhash)
-
-	sha := sha256.New()
-	shahash := sha.Sum(p.D.Bytes())
-	fmt.Printf("sha  %d %x\n", len(shahash), shahash)
-
-	md := md5.New()
-	mdhash := md.Sum(p.D.Bytes())
-	fmt.Printf("md5  %d %x\n", len(mdhash), mdhash)
-
-	hx := make([]byte, hex.EncodedLen(len(p.D.Bytes())))
-	hex.Encode(hx, p.D.Bytes())
-	fmt.Printf("hex  %d %x\n", len(hx), hx)
-
-	b58 := btcutil.Base58Encode(p.D.Bytes())
-	fmt.Printf("b58  %d %s\n", len(b58), b58)
-
-	ex := "2osLAeuhKKwu61eu7MnXpbGU7Rkb6j155aXo515mWoS91nDPRR5rJGgvG3VRGpPpGWo4AEU3HmqtsQUcuPm8aBancYp5kC81gWpY7PCPa7cFZg"
-	fmt.Printf("ex  %d %s\n", len(ex), ex)*/
-
-	return []byte{}, []byte{}, err
+	return priv, pub, err
 }
 
-func Public(X, Y *big.Int) []byte {
+type PublicKey []byte
+
+const (
+	MAIN      = 0x01
+	TESTNET   = 0x11
+	EPHEMERAL = 0x02
+)
+
+// Sin accepts as input PublicKey and returns a SIN (Secure Identity Number)
+// Learn more: https://en.bitcoin.it/wiki/Identity_protocol_v1
+func Sin(key PublicKey) []byte {
+	s := (sha256.New()).Sum(key)
+	// Type1 persistant keys prefix with main 0x01, testnet 0x11
+	// Type2 ephemeral 0x02
+	bt := 0x02
+	fmt.Println(Sin)
+	_, _ = s, bt
+	return []byte{}
+}
+
+// Accepts the X, Y point and returns the Public Key
+func public(X, Y *big.Int) []byte {
 
 	//sha256_h := sha256.New()
 	/* Create a new RIPEMD160 Context */
@@ -60,4 +56,26 @@ func Public(X, Y *big.Int) []byte {
 	/* 1. Prepend 0x04 */
 	return append([]byte{0x04}, pubkey_bytes...)
 
+}
+
+func PublicFromPrivate(priv []byte) []byte {
+	d := new(big.Int).SetBytes(priv)
+	curve := getCurve()
+	Q := curve.Point_scalar_multiply(d, curve.G)
+	//fmt.Printf("% #v\n", pretty.Formatter(Q))
+	return public(Q.X, Q.Y)
+}
+
+func getCurve() *btcaddr.EllipticCurve {
+	/* secp256k1 elliptic curve parameters */
+	var curve = &btcaddr.EllipticCurve{}
+	curve.P, _ = new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
+	curve.A, _ = new(big.Int).SetString("0000000000000000000000000000000000000000000000000000000000000000", 16)
+	curve.B, _ = new(big.Int).SetString("0000000000000000000000000000000000000000000000000000000000000007", 16)
+	curve.G.X, _ = new(big.Int).SetString("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16)
+	curve.G.Y, _ = new(big.Int).SetString("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16)
+	curve.N, _ = new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16)
+	curve.H, _ = new(big.Int).SetString("01", 16)
+
+	return curve
 }
--- key_test.go
+++ key_test.go
@@ -1,6 +1,7 @@
 package go_bitpay_client
 
 import (
+	"encoding/hex"
 	"fmt"
 	"log"
 	"testing"
@@ -8,10 +9,23 @@ import (
 
 func TestKeygen(t *testing.T) {
 
-	pub, priv, err := Keygen()
+	priv, pub, err := Keygen()
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = priv
-	fmt.Println(len(pub), pub)
+	_, _ = priv, pub
+	fmt.Printf("Private: %x\n", priv)
+	fmt.Printf("Public: %x\n", pub)
+	// The output is random, so needs a test validating the
+	// pub and priv key are connected.
+}
+
+func TestSin(t *testing.T) {
+	s := "a72e6ed895e9ec2621bcccb93a11b73100887bdbb684cbfa6241b6076c54d0b4"
+	s = "a72e6ed895e9ec2621bcccb93a11b73100887bdbb684cbfa6241b6076c54d0b4"
+	src, _ := hex.DecodeString(s)
+	pub := PublicFromPrivate([]byte(src))
+	sin := Sin(pub)
+	fmt.Printf("Public: %x\n", pub)
+	fmt.Printf("Sin: %x\n", sin)
 }
