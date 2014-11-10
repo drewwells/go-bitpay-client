19d950d101f1e2431dd3880b4a65c15637c3401d
Drew Wells <drew.wells00@gmail.com>
2014-11-09 22:33:37 -0600

    Add benchmarks, elliptic algos are too slow

--- key_test.go
+++ key_test.go
@@ -7,9 +7,7 @@ import (
 	"testing"
 )
 
-const (
-	TESTPRIV = "1f0dd50dd70a6fce3fbe81b922d89610742b37aa1eae9f82ffecb5571dcebfa4"
-)
+var TESTPRIV = []byte("1f0dd50dd70a6fce3fbe81b922d89610742b37aa1eae9f82ffecb5571dcebfa4")
 
 func TestKeygen(t *testing.T) {
 
@@ -18,6 +16,7 @@ func TestKeygen(t *testing.T) {
 		log.Fatal(err)
 	}
 	_, _ = priv, pub
+
 	// fmt.Printf("Private: %x\n", priv)
 	// fmt.Printf("Public: %x\n", pub)
 
@@ -25,9 +24,18 @@ func TestKeygen(t *testing.T) {
 	// pub and priv key are connected.
 }
 
+func BenchmarkKeygen(b *testing.B) {
+	priv := make([]byte, 32)
+	pub := make([]byte, 33)
+	for n := 0; n < b.N; n++ {
+		priv, pub, _ = Keygen()
+	}
+	dummy, dummy = priv, pub
+}
+
 func TestPublicFromPrivate(t *testing.T) {
-	full := hex.EncodeToString(PublicFromPrivate([]byte(TESTPRIV), false))
-	comp := hex.EncodeToString(PublicFromPrivate([]byte(TESTPRIV), true))
+	full := hex.EncodeToString(PublicFromPrivate(TESTPRIV, false))
+	comp := hex.EncodeToString(PublicFromPrivate(TESTPRIV, true))
 
 	eful := "047dc432eef530a7d46066921af445ac67ae3147e93131910c1578c813af06375ef9b0e9987c243f0a78df60eac6f9c777f2ba66e57936a9bb1dccb76fead0c4a0"
 	ecom := "027dc432eef530a7d46066921af445ac67ae3147e93131910c1578c813af06375e"
@@ -42,11 +50,28 @@ func TestPublicFromPrivate(t *testing.T) {
 	}
 }
 
-func ExampleSin() {
+var dummy []byte
 
-	src, _ := hex.DecodeString(TESTPRIV)
-	pub := PublicFromPrivate(src, false)
-	pubcomp := PublicFromPrivate(src, true)
+func BenchmarkPublicFromPrivate_Compressed(b *testing.B) {
+	var bs []byte
+	for n := 0; n < b.N; n++ {
+		bs = PublicFromPrivate(TESTPRIV, true)
+	}
+	dummy = bs
+}
+
+func BenchmarkPublicFromPrivate_Uncompressed(b *testing.B) {
+	var bs []byte
+	for n := 0; n < b.N; n++ {
+		bs = PublicFromPrivate(TESTPRIV, false)
+	}
+	dummy = bs
+}
+func ExampleSin() {
+	txt := make([]byte, hex.DecodedLen(len(TESTPRIV)))
+	hex.Decode(txt, TESTPRIV)
+	pub := PublicFromPrivate(txt, false)
+	pubcomp := PublicFromPrivate(txt, true)
 
 	sin := Sin(pubcomp)
 
